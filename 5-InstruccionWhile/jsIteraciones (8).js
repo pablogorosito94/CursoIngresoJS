4f8fabd5170a869f339d32b71359a3e99a7be7de
Gorosito Pablo <pablogorosito94@gmail.com>
2017-07-12 22:10:24 -0300

    commit del 8
    lo modifiquye

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -12,17 +12,25 @@ function Mostrar()
 		if (valor>0)
 		{
 			positivo=positivo+valor
+			contador=contador+1
 		}
 		else
 		{
 			negativo=negativo*valor
+			contador=contador+1
 		}
 		respuesta=prompt("Quiere ingresar mas numeros? <si/no>");
 		contador=contador+1
 	}
+	if (contador==0)
+	{
+		
+		document.getElementById('producto').value=0;
+			else:
+
+	}
 
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+
 
 }//FIN DE LA FUNCIÓN
